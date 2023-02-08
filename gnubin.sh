14fd9fcf5268d779c3151d604974a7a0d88bb90c
Tommy Wang <twang@august8.net>
2023-02-08 11:22:49 -0600

    add brew itself to path on m1

--- gnubin.sh
+++ gnubin.sh
@@ -3,6 +3,7 @@ if [[ -d /usr/local/opt ]] || [[ -d /opt/homebrew ]]; then
         BREW_PATH=/usr/local/opt
     elif [ "$(uname -m)" = "arm64" ]; then
         BREW_PATH=/opt/homebrew
+        echo "export PATH=\"$BREW_PATH/bin:$PATH\""
     fi
     GNUBIN=$(find -L $BREW_PATH -type d -name gnubin | paste -sd':' -)
     GNUMAN=$(find -L $BREW_PATH -type d -name gnuman | paste -sd':' -)
