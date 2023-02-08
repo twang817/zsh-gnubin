7ecc784c841daf6f13967f487e14162aee018814
Tommy Wang <twang@august8.net>
2023-02-08 10:06:34 -0600

    update for m1

--- gnubin.sh
+++ gnubin.sh
@@ -1,6 +1,11 @@
 [ -d /usr/local/opt ] && {
-    GNUBIN=$(find -L /usr/local/opt -type d -name gnubin | paste -sd':' -)
-    GNUMAN=$(find -L /usr/local/opt -type d -name gnuman | paste -sd':' -)
+    if [ "$(uname -m)" = "x86_64" ]; then
+        BREW_PATH=/usr/local/opt
+    elif [ "$(uname -m)" = "arm64" ]; then
+        BREW_PATH=/opt/homebrew
+    fi
+    GNUBIN=$(find -L $BREW_PATH -type d -name gnubin | paste -sd':' -)
+    GNUMAN=$(find -L $BREW_PATH -type d -name gnuman | paste -sd':' -)
 }
 [ ! -z "$GNUBIN" ] && echo "export PATH=\"$GNUBIN:\$PATH\""
 [ ! -z "$GNUMAN" ] && echo "export MANPATH=\"$GNUMAN:\$MANPATH\""
